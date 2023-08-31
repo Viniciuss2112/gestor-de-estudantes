a4fd775e30d63ff80cdc0040ffcf5273b3d547d2
Viniciuss2112 <118479841+Viniciuss2112@users.noreply.github.com>
2023-08-31 16:53:07 -0300

    Atualização

---
+++ sistema-gestao-estudantes/AtualizarDeletarEstudante.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sistema_gestao_estudantes
+{
+    public partial class AtualizarDeletarEstudante : Form
+    {
+        public AtualizarDeletarEstudante()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonEnviarFoto_Click(object sender, EventArgs e)
+        {
+            // Pesquisa pela imagem no computador.
+            OpenFileDialog abrirArquivo = new OpenFileDialog();
+            abrirArquivo.Filter =
+                "Seleciona a Foto(*.jpg;*.png;*.gif)|*.jpg;*.png;*.gif";
+            if (abrirArquivo.ShowDialog() == DialogResult.OK)
+            {
+                pictureBoxFoto.Image = Image.FromFile(abrirArquivo.FileName);
+            }
+        }
+
+        private void buttonConfirmar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
